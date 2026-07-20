c737d9b0f62a69f8425000ce20a83dc75ae601fe
Nangaaoay246 <jan.michael.aoay246@adamson.edu.ph>
2026-07-20 16:25:08 +0800

    refactor code

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 import streamlit as st
+from data import profile, links
 import PIL as image
 import webbrowser
-
-PROFILE = "assets\profile.png"
-NAME = "Jan Michael Aoay"
-TITLE = "👨‍💻 Python Developer & ML Engineer"
-LOCATION = "🌏 Cavite, Philippines"
-EMAIL = "✉️ aoay.janmichael@gmail.com"
-PHONE = "📞 0976 339 2122"
 
 def configure():
     st.set_page_config(
@@ -18,26 +12,52 @@ def configure():
     ) 
 
 def load_css():
+    st.markdown("""
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    """, unsafe_allow_html=True)
+
     with open("styles.css") as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+        st.markdown(
+            f"<style>{f.read()}</style>",
+            unsafe_allow_html=True
+        )
 
 def sidebar():
     with st.sidebar:
-        st.image(PROFILE, width=160)
-        st.html(f"""
+        st.markdown(f"""
             <div style="text-align: center;">
-                <h2>{NAME}</h2>
-                <p style="color: var(--accent-color)">{TITLE}</p>
+                <img src="{profile['image_path']}" style="border-radius: 50%; width: 160px; height: 160px; object-fit: cover; margin-bottom: 1rem;">
+                <h2>{profile['name']}</h2>
+                <p style="color: var(--accent-color)">{profile['title']}</p>
             </div>
-        """)
+        """, unsafe_allow_html=True)
+        
         with st.expander("📫 Contact ", expanded=True):
             st.html(f"""
-                <p>{LOCATION}</p>
-                <p>{PHONE}</p>
-                <p>{EMAIL}</p>
+                <p>{profile['location']}</p>
+                <p>{profile['phone']}</p>
+                <p>{profile['email']}</p>
             """)
 
+        social_links()
 
+def social_links():
+    link_html = "".join(
+        f"""
+        <a href="{link['url']}" target="_blank">
+            <i class="{link['icon']}"></i>
+        </a>
+        """
+        for link in links)
+
+    st.markdown(
+        f"""
+        <div style="text-align:center; margin:2rem 2rem;">
+            {link_html}
+        </div>
+        """, unsafe_allow_html=True,
+    )
 
 def main():
     configure()
---
+++ data.py
@@ -0,0 +1,21 @@
+profile = {
+    "name": "Jan Michael Aoay",
+    "title": "👨‍💻 Python Developer & ML Engineer",
+    "image_path": "assets/profile.png",
+    "location": "🌏 Cavite, Philippines",
+    "email": "✉️ aoay.janmichael@gmail.com",
+    "phone": "📞 0976 339 2122",
+}
+
+links = [
+    {
+        "name": "LinkedIn",
+        "url": "https://www.linkedin.com/in/jan-michael-aoay/",
+        "icon": "fab fa-linkedin"
+    },
+    {
+        "name": "GitHub",
+        "url": "https://github.com/Nangaaoay246",
+        "icon": "fab fa-github"
+    },
+]
