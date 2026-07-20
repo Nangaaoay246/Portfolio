fc904d967f071136563cfcf09627fbfc03edaf77
Nangaaoay246 <jan.michael.aoay246@adamson.edu.ph>
2026-07-20 16:27:42 +0800

    changed image_path to raw link

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 profile = {
     "name": "Jan Michael Aoay",
     "title": "👨‍💻 Python Developer & ML Engineer",
-    "image_path": "assets/profile.png",
+    "image_path": "https://raw.githubusercontent.com/Nangaaoay246/Portfolio/refs/heads/main/assets/profile.png",
     "location": "🌏 Cavite, Philippines",
     "email": "✉️ aoay.janmichael@gmail.com",
     "phone": "📞 0976 339 2122",
