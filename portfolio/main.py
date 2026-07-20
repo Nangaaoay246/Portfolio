583ecae273f369b25b0b7730fd86c0967bfb2b21
Nangaaoay246 <jan.michael.aoay246@adamson.edu.ph>
2026-07-20 14:46:12 +0800

    configure page

--- portfolio/main.py
+++ portfolio/main.py
@@ -1,3 +1,11 @@
 import streamlit as st
 from PIL import Image
 import webbrowser
+
+# Configure
+st.set_page_config(
+    page_title='Portfolio - Jan Michael Aoay',
+    page_icon='👾',
+    layout='wide',
+    initial_sidebar_state='expanded'
+)
